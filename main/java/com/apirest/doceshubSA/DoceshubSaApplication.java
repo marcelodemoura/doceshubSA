ae79cbca39a6b1b5659513cbbdd87f2a1d126d9c
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-04-10 10:16:55 -0300

    first commit

---
+++ main/java/com/apirest/doceshubSA/DoceshubSaApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.doceshubSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoceshubSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoceshubSaApplication.class, args);
+	}
+
+}
